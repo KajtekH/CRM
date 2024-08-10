65f1942b87faa218785f96ff5aefc3db372f8adf
KajtekH <kajhulboj@wp.pl>
2024-08-10 21:57:48 +0200

    Add CourseDTO

---
+++ CRM - microservices/Course Service/src/main/java/com/kajtekh/courseservice/Model/CourseDTO.java
@@ -0,0 +1,10 @@
+package com.kajtekh.courseservice.Model;
+
+import java.time.LocalDate;
+
+public record CourseDTO(Long id,
+                        String title,
+                        String description,
+                        LocalDate startDate,
+                        LocalDate endDate) {
+}
