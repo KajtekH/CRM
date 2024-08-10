2c3dcf4765dc8cdc34385653d43ec53dccf8139d
KajtekH <kajhulboj@wp.pl>
2024-08-10 23:12:37 +0200

    Add course services
    -addCourse
    -getAllCourses
    -getCoursesByType

--- CRM - microservices/Course Service/src/main/java/com/kajtekh/courseservice/Repository/CourseRepository.java
+++ CRM - microservices/Course Service/src/main/java/com/kajtekh/courseservice/Repository/CourseRepository.java
@@ -1,7 +1,15 @@
 package com.kajtekh.courseservice.Repository;
 
 import com.kajtekh.courseservice.Model.Course;
+import com.kajtekh.courseservice.Model.CourseType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+
+    List<Course> findAll();
+    List<Course> findByCourseType(CourseType courseType);
 }
--- CRM - microservices/Course Service/src/main/java/com/kajtekh/courseservice/Service/CourseService.java
+++ CRM - microservices/Course Service/src/main/java/com/kajtekh/courseservice/Service/CourseService.java
@@ -1,6 +1,15 @@
 package com.kajtekh.courseservice.Service;
 
+import com.kajtekh.courseservice.Model.Course;
+import com.kajtekh.courseservice.Model.CourseType;
+import com.kajtekh.courseservice.Model.DTO.CourseDTO;
+import com.kajtekh.courseservice.Model.DTO.CreateCourseDTO;
 import com.kajtekh.courseservice.Repository.CourseRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Iterator;
+import java.util.List;
 
 public class CourseService {
 
@@ -9,4 +18,26 @@ public class CourseService {
     public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
+
+    @Transactional
+    public Course addCourse(CreateCourseDTO createCourseDTO) {
+        return courseRepository.save(CourseMapper.mapToCourse(createCourseDTO));
+    }
+
+    @Transactional(readOnly = true)
+    public List<CourseDTO> getAllCourses() {
+        return courseRepository.findAll()
+                .stream()
+                .map(CourseMapper::mapToCourseDTO)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<CourseDTO> getCoursesByType(CourseType courseType) {
+        return courseRepository.findByCourseType(courseType)
+                .stream()
+                .map(CourseMapper::mapToCourseDTO)
+                .toList();
+    }
+
 }
