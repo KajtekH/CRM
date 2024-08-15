ca98139da2366fcbf0265d3bcf8410e78ec7c239
KajtekH <kajhulboj@wp.pl>
2024-08-15 20:59:49 +0200

    Add UserService

---
+++ CRM - microservices/User Service/src/main/java/com/kajtekh/userservice/service/UserService.java
@@ -0,0 +1,33 @@
+package com.kajtekh.userservice.service;
+
+
+import com.kajtekh.userservice.model.User;
+import com.kajtekh.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+}
