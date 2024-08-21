54a7a8902199b67a9927662b3a5e068fd76a6e69
KajtekH <kajhulboj@wp.pl>
2024-08-22 01:43:45 +0200

    Add TrainerDTOs

---
+++ Trainer Service/src/main/java/com/kajtekh/trainerservice/model/dto/PreviewTrainerDTO.java
@@ -0,0 +1,5 @@
+package com.kajtekh.trainerservice.model.dto;
+
+public record PreviewTrainerDTO(String firstName,
+                                String lastName) {
+}
--- Trainer Service/src/main/java/com/kajtekh/trainerservice/model/dto/TrainerDTO.java
+++ Trainer Service/src/main/java/com/kajtekh/trainerservice/model/dto/TrainerDTO.java
@@ -2,7 +2,8 @@ package com.kajtekh.trainerservice.model.dto;
 
 import java.util.Optional;
 
-public record TrainerDTO(Long userId,
+public record TrainerDTO(Long id,
+                         Long userId,
                          String firstName,
                          String lastName,
                          Optional<String> bio,
